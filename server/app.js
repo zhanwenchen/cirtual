f22cd63eff2d7a02befe4560a3c1bab70b661a9c
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-03-21 11:27:40 -0400

    passport code compiled

--- server/app.js
+++ server/app.js
@@ -1,63 +1,16 @@
 // server/app.js
 // router config
 
-const express = require('express');
-const path = require('path');
-// const favicon = require('serve-favicon');
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
-const routes = require('./routes/index');
-// var users = require('./routes/users');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+var app = express();
 
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-
-const app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -65,14 +18,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+// Initialize Passport and restore any authentication state from session
 app.use(passport.initialize());
 app.use(passport.session());
 
 
+// Using the flash middleware to store messages in session
+// and displaying in templates
+var flash = require('connect-flash');
+app.use(flash());
+
+
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+
+// Send routes into passport middleware
+var routes = require('./routes/index')(passport);
 app.use('/', routes);
-// app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -13,125 +13,141 @@ const express = require('express');
 const app = express.Router();
 const path = require('path');
 const sequelize = require('sequelize');
-const passport = require('passport');
 var models = require('../models/index');
 
-// View root page
-app.get('/', (req, res, next) => {
-  console.log("./ = %s", path.resolve("./"));
-  console.log("__dirname = %s", path.resolve(__dirname));
-  res.sendFile(path.join(
-    __dirname, '..', '..', 'client', 'views', 'index.html'));
-});
 
-// login function
-app.post('/login',
-  passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      successFlash: 'Welcome!',
-      failureFlash: 'Invalid username or password.'
-    }
-  )
-);
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
 
-// logout function with passport
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
 
-// JSON endpoint: Search a single user
-app.get('/user/:id', function(req, res) {
-  models.User.find({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(user) {
-    res.json(user);
+module.exports = function(passport){
+
+  // View root page
+  app.get('/', (req, res, next) => {
+    console.log("./ = %s", path.resolve("./"));
+    console.log("__dirname = %s", path.resolve(__dirname));
+    res.sendFile(path.join(
+      __dirname, '..', '..', 'client', 'views', 'index.html'));
   });
-});
 
-// JSON endpoint: List all users
-app.get('/users', function(req, res) {
-  models.User.findAll({}).then(function(users) {
-    res.json(users);
+  // login function
+  app.post('/login',
+    passport.authenticate('login', {
+        successRedirect: '/home',
+        failureRedirect: '/',
+        successFlash: 'Welcome!',
+        failureFlash: 'Invalid username or password.'
+      }
+    )
+  );
+
+  // logout function with passport
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
   });
-});
 
-
-// JSON endpoint: Create a new user
-app.post('/user', function(req, res) {
-  models.User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password
-  }).then(function(user) {
-    res.json(user);
+  // JSON endpoint: Search a single user
+  app.get('/user/:id', function(req, res) {
+    models.User.find({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(user) {
+      res.json(user);
+    });
   });
-});
 
-
-// JSON endpoint: Update a single user
-app.put('/user/:id', function(req, res) {
-  models.User.find({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(user) {
-    if(user){
-      user.updateAttributes({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(function(user) {
-        res.json(user);
-      });
-    }
+  // JSON endpoint: List all users
+  app.get('/users', function(req, res) {
+    models.User.findAll({}).then(function(users) {
+      res.json(users);
+    });
   });
-});
 
 
-// JSON endpoint: Delete a single user
-app.delete('/user/:id', function(req, res) {
-  models.User.find({
-    where: {
-      id: req.params.id
-    }
-  }).then( (user) => {
-    if(user){
-      var deletedUser = user;
-      user.destroy().then( () => {
-        res.json(deletedUser);
-      });
-    }
+  // JSON endpoint: Create a new user
+  app.post('/user', function(req, res) {
+    models.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    }).then(function(user) {
+      res.json(user);
+    });
   });
-});
 
-// JSON endpoint: Retrieve a single message
-app.get('/message/:id', function(req, res) {
-  models.Message.find({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(message) {
-    res.json(message);
+
+  // JSON endpoint: Update a single user
+  app.put('/user/:id', function(req, res) {
+    models.User.find({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(user) {
+      if(user){
+        user.updateAttributes({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password,
+        }).then(function(user) {
+          res.json(user);
+        });
+      }
+    });
   });
-});
 
 
-// JSON endpoint: Create a single message
-app.post('/message/new/:room', function(req, res) {
-  models.Message.create({
-    text: req.body.text,
-    UserId: req.body.user_id,
-    room: req.params.room
-  }).then(function(message) {
-    res.json(message);
+  // JSON endpoint: Delete a single user
+  app.delete('/user/:id', function(req, res) {
+    models.User.find({
+      where: {
+        id: req.params.id
+      }
+    }).then( (user) => {
+      if(user){
+        var deletedUser = user;
+        user.destroy().then( () => {
+          res.json(deletedUser);
+        });
+      }
+    });
   });
-});
+
+  // JSON endpoint: Retrieve a single message
+  app.get('/message/:id', function(req, res) {
+    models.Message.find({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(message) {
+      res.json(message);
+    });
+  });
 
 
-module.exports = app;
+  // JSON endpoint: Create a single message
+  app.post('/message/new/:room', function(req, res) {
+    models.Message.create({
+      text: req.body.text,
+      UserId: req.body.user_id,
+      room: req.params.room
+    }).then(function(message) {
+      res.json(message);
+    });
+  });
+
+  return app;
+}
+
+
+// module.exports = app;
